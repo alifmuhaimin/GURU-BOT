0b02bd877d89dd3d5131e092bcc0a6ba76c3260b
alifmuhaimin <157699684+alifmuhaimin@users.noreply.github.com>
2024-01-25 20:02:40 +0800

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,7 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
-  ['6281255369012', 'Ash', true], 
+  ['60194178104', 'liff', true], 
   [''] 
 ] //Number of owners
 
